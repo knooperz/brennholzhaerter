d88be85f81925ffc00304bdf5aa54df45f4890de
john99.herter <john99.herter@gmail.com>
2023-04-29 13:26:34 +0200

    SocialMedia Bilder hinzufügen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { BrennholzbestellungComponent } from './brennholzbestellung/brennholzbestellung.component';
 import { DienstleistungenComponent } from './dienstleistungen/dienstleistungen.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { KontaktComponent } from './kontakt/kontakt.component';
 import { LayoutComponent } from './layout/layout.component';
 import { ProduktComponent } from './produkt/produkt.component';
 import { StartseiteComponent } from './startseite/startseite.component';
@@ -15,8 +17,9 @@ const routes: Routes = [
         { path: 'brennholzbestellung', component: BrennholzbestellungComponent },
         { path: 'dienstleistungen/produkt', component: ProduktComponent },
         { path: 'ueberuns', component: AboutUsComponent },
-        { path: 'dienstleistungen', component: DienstleistungenComponent}
-
+        { path: 'dienstleistungen', component: DienstleistungenComponent},
+        { path: 'kontakt', component: KontaktComponent},
+        { path: 'impressum', component: ImpressumComponent}
       ]
   }
 ];
